e71be662dba0e70b7ac56f8d537dd7eed147d281
ChauVietKhanhTrungK18 <trungcvkse181536@fpt.edu.vn>
2026-06-01 18:53:58 +0700

    ExportExcel

--- StudentGradeManagement/Program.cs
+++ StudentGradeManagement/Program.cs
@@ -2,8 +2,11 @@ using BusinessLayer.IService;
 using BusinessLayer.Services;
 using DataAccessLayer.DbContexts;
 using DataAccessLayer.FileHandlers.FG;
+using DataAccessLayer.IRepository;
+using DataAccessLayer.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using OfficeOpenXml;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -59,10 +62,15 @@ builder.Services.AddSwaggerGen(option =>
         }
     });
 });
+ExcelPackage.License.SetNonCommercialPersonal("StudentGradeManagement");
+
 builder.Services.AddScoped<BusinessLayer.Mapping.FGMapper>();
 builder.Services.AddScoped<IFGImportService,FGImportService>();
 builder.Services.AddScoped<IFGReader, FGReader>();
 builder.Services.AddScoped<ISubjectService, SubjectService>();
+builder.Services.AddScoped<IExcelService, ExcelService>();
+builder.Services.AddScoped<IExcelRepository,ExcelRepository>();
+
 var app = builder.Build();
 
 app.UseCors("AllowAll");
